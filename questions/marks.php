9c47e18411d4bf30c0848a1e9c3e2aefc0b4cad2
Afrie Irham <itsafrie@gmail.com>
2019-05-25 01:31:23 +0800

    Question design retouch

--- questions/marks.php
+++ questions/marks.php
@@ -35,15 +35,29 @@ echo $stopTime = $_POST['stopTime'];
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Thank you for your time!</title>
+
+    <style>
+        body {
+            background-image: url("images_q/fire.gif");
+            background-size: cover;
+            height: 100%;
+        }
+
+        #cmain {
+
+            background-color: rgba(206, 221, 228, 0.6);
+            color: #394652;
+        }
+    </style>
 </head>
 
 <body>
     <div class="container mt-5 text-center">
-        <h1 class="h1">Thank you for your time</h1>
-        <div class="card mt-5">
+        <h1 class="h1 text-white">Thank you for your time</h1>
+        <div class="card bg-dark text-white mt-5">
             <div class="card-body">
                 <p style="font-size : 40px"> Summary of your attempts : <strong> 80 %</strong></p>
-                <table class="table table-striped">
+                <table class="table table-dark table-striped table-bordered">
                     <tbody>
                         <tr>
                             <th>Started on</th>
@@ -77,13 +91,13 @@ echo $stopTime = $_POST['stopTime'];
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
+        </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-    </script>
+        </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
+        </script>
 </body>
 
 </html>
