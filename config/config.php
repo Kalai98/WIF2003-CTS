db8770f145b4a2f93fccb0e942deebcaeb0b780f
afrie <itsafrie@gmail.com>
2020-11-19 00:22:36 +0800

    update config

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
-    define('ROOT_URL', 'http://localhost/phpsandbox/cts-project/');
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', 'admin');
-    define('DB_NAME', 'cts-db');
+    define('ROOT_URL', 'http://localhost:8888/cts/');
+    define('DB_HOST', '127.0.0.1');
+    define('DB_USER', 'dev');
+    define('DB_PASS', 'mysql@localhost');
+    define('DB_NAME', 'cts');
